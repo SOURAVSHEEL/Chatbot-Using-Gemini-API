402972dc14cd9f584cfc4d51d7d918201dbd8997
Sourav Kumar Sheel <122149563+SOURAVSHEEL@users.noreply.github.com>
2024-04-16 01:19:18 +0530

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,15 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# app_name = 'chatbot'
-
-# urlpatterns = [
-#     path('', views.chat_text, name='chat_text'),
-#     path('',views.chat_image, name='chat_image'),
-# ]
-
-# urls.py
-
 from django.urls import path
 from .views import chat_text, chat_image, home
 
